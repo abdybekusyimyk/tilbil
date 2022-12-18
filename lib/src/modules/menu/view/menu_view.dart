044a9861a9e10109eea2d336e6395d806e9701d1
abdybekusyimyk <abdybekuulusyimyk@gmail.com>
2022-12-18 17:42:27 +0600

    menu

--- lib/src/modules/menu/view/menu_view.dart
+++ lib/src/modules/menu/view/menu_view.dart
@@ -1,19 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tilbil/src/app_constants/app_spaces.dart';
 
 class MenuView extends StatelessWidget {
-  const MenuView({Key? key}) : super(key: key);
+  const MenuView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            //   Image.asset("assets/images/oymoysty.png"),
-            //   Image.asset("assets/images/oymo.png"),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Image.asset(
+            'assets/images/oymoysty.png',
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 45),
+            child: Column(
+              children: const [
+                InfoWidget(),
+                AppSpace.sized25,
+                InfoWidget(),
+                AppSpace.sized25,
+                InfoWidget(),
+                AppSpace.sized25,
+                InfoWidget(),
+              ],
+            ),
+          ),
+          Image.asset(
+            'assets/images/oymo.png',
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class InfoWidget extends StatelessWidget {
+  const InfoWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Row(
+            children: [
+              SvgPicture.asset('assets/icons/biz.svg'),
+              AppSpace.sizedW25,
+              const Text(
+                'Биз жөнүндө',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+        const Divider(
+          thickness: 1.2,
+          color: Colors.blue,
+        )
+      ],
     );
   }
 }
