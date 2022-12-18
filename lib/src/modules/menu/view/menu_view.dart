2cb845c315a7479fc3f049cb844b60047426b415
abdybekusyimyk <abdybekuulusyimyk@gmail.com>
2022-12-18 17:54:02 +0600

    info

--- lib/src/modules/menu/view/menu_view.dart
+++ lib/src/modules/menu/view/menu_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tilbil/src/app_constants/app_spaces.dart';
+import 'package:tilbil/src/app_widgets/others/info_widget.dart';
 
 class MenuView extends StatelessWidget {
   const MenuView({super.key});
@@ -33,37 +33,6 @@ class MenuView extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class InfoWidget extends StatelessWidget {
-  const InfoWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Row(
-            children: [
-              SvgPicture.asset('assets/icons/biz.svg'),
-              AppSpace.sizedW25,
-              const Text(
-                'Биз жөнүндө',
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-        ),
-        const Divider(
-          thickness: 1.2,
-          color: Colors.blue,
-        )
-      ],
     );
   }
 }
